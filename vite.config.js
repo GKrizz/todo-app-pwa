2fe096f1f0e9ffcdb94580e3089428eec30d0e89
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2026-05-03 00:16:03 +0530

    made change

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base:"/todo-app-pwa/",
-  plugins: [react()],
+  base: '/', // ✅ IMPORTANT for Vercel
+
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'gobal1.png'],
+      manifest: {
+        name: 'Todo App',
+        short_name: 'Todo',
+        description: 'Todo App PWA',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#1d4ed8',
+        icons: [
+          {
+            src: 'gobal1.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'gobal1.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
