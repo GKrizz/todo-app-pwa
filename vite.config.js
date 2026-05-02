7b8b9d5bec6cabee80bab7867bfe59e47f082e8b
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2026-05-03 00:04:40 +0530

    made change in package.json file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Todo_App/",
+  base:"/todo-app-pwa/",
   plugins: [react()],
 })
